74a99a16bd4f95031e454e76cf18668578efffbc
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-20 14:40:34 -0300

    Finalizada Fragment Bichos

--- app/src/main/java/com/talissonmelo/aprendaingles/Fragments/BichosFragment.java
+++ app/src/main/java/com/talissonmelo/aprendaingles/Fragments/BichosFragment.java
@@ -1,19 +1,25 @@
 package com.talissonmelo.aprendaingles.Fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.talissonmelo.aprendaingles.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class BichosFragment extends Fragment {
+public class BichosFragment extends Fragment implements  View.OnClickListener{
+
+    private ImageView buttonCao, buttonGato, buttonLeao, buttonMacao, buttonOvelha, buttonVaca;
+    private MediaPlayer mediaPlayer;
 
     public BichosFragment() {
         // Required empty public constructor
@@ -24,6 +30,71 @@ public class BichosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bichos, container, false);
+        View view = inflater.inflate(R.layout.fragment_bichos, container, false);
+
+        buttonCao = view.findViewById(R.id.buttonCao);
+        buttonGato = view.findViewById(R.id.buttonGato);
+        buttonLeao = view.findViewById(R.id.buttonLeao);
+        buttonMacao = view.findViewById(R.id.buttonMacaco);
+        buttonOvelha = view.findViewById(R.id.buttonOvelha);
+        buttonVaca = view.findViewById(R.id.buttonVaca);
+
+        //Aplicando Evento de Click
+        buttonCao.setOnClickListener(this);
+        buttonGato.setOnClickListener(this);
+        buttonLeao.setOnClickListener(this);
+        buttonMacao.setOnClickListener(this);
+        buttonOvelha.setOnClickListener(this);
+        buttonVaca.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.buttonCao:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.dog);
+                tocarSom();
+                break;
+
+            case R.id.buttonGato:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.cat);
+                tocarSom();
+                break;
+
+            case R.id.buttonLeao:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.lion);
+                tocarSom();
+                break;
+
+            case R.id.buttonMacaco:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.monkey);
+                tocarSom();
+                break;
+
+            case R.id.buttonOvelha:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.sheep);
+                tocarSom();
+                break;
+
+            case R.id.buttonVaca:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.cow);
+                tocarSom();
+                break;
+        }
+    }
+
+    public void tocarSom(){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.release();
+                }
+            });
+        }
     }
 }
