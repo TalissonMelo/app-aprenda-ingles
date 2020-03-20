da066984fd177e0311e1959336e7cf4ef4bead72
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-03-20 15:31:26 -0300

    Projeto Finalizado

--- app/src/main/java/com/talissonmelo/aprendaingles/Fragments/BichosFragment.java
+++ app/src/main/java/com/talissonmelo/aprendaingles/Fragments/BichosFragment.java
@@ -97,4 +97,13 @@ public class BichosFragment extends Fragment implements  View.OnClickListener{
             });
         }
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
 }
--- app/src/main/java/com/talissonmelo/aprendaingles/Fragments/NumerosFragment.java
+++ app/src/main/java/com/talissonmelo/aprendaingles/Fragments/NumerosFragment.java
@@ -1,5 +1,6 @@
 package com.talissonmelo.aprendaingles.Fragments;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,17 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.talissonmelo.aprendaingles.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class NumerosFragment extends Fragment {
+public class NumerosFragment extends Fragment implements  View.OnClickListener{
+
+    private ImageView button1, button2, button3, button4, button5, button6;
+    private MediaPlayer mediaPlayer;
 
     public NumerosFragment() {
         // Required empty public constructor
@@ -24,6 +29,78 @@ public class NumerosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_numero, container, false);
+        View view = inflater.inflate(R.layout.fragment_numero, container, false);
+
+        button1 = view.findViewById(R.id.button1);
+        button2 = view.findViewById(R.id.button2);
+        button3 = view.findViewById(R.id.button3);
+        button4 = view.findViewById(R.id.button4);
+        button5 = view.findViewById(R.id.button5);
+        button6 = view.findViewById(R.id.button6);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+        button5.setOnClickListener(this);
+        button6.setOnClickListener(this);
+
+        return view;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button1:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.one);
+                tocarSom();
+                break;
+
+            case R.id.button2:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.two);
+                tocarSom();
+                break;
+
+            case R.id.button3:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.three);
+                tocarSom();
+                break;
+
+            case R.id.button4:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.four);
+                tocarSom();
+                break;
+
+            case R.id.button5:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.five);
+                tocarSom();
+                break;
+
+            case R.id.button6:
+                mediaPlayer = MediaPlayer.create(getActivity(), R.raw.six);
+                tocarSom();
+                break;
+        }
+    }
+
+    public void tocarSom(){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mediaPlayer) {
+                    mediaPlayer.release();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(mediaPlayer != null){
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
     }
 }
